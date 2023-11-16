dd42387828f52d3bf855095d9a765cef3fe36273
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-11-16 20:38:14 +0600

    refactor: change the site name

--- components/loader.tsx
+++ components/loader.tsx
@@ -5,13 +5,13 @@ export const Loader = () => {
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
       <div className="w-10 h-10 relative animate-spin">
         <Image
-          alt="Logo"
-          src="/logo.png"
+          alt="Tars"
+          src="/tars.png"
           fill
         />
       </div>
       <p className="text-sm text-muted-foreground">
-        Genius is thinking...
+        Tars is thinking...
       </p>
     </div>
   );
