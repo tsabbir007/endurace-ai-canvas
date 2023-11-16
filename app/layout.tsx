7fbdf6b2bef5ef670d84f742e5f2bcdca1ec2c80
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-11-17 00:13:24 +0600

    feat: add website title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import {ClerkProvider} from "@clerk/nextjs";
 
+
 import "./globals.css";
 
 const font = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-    title: "AI Canvas",
-    description: "AI Platform"
+    title: "Endurance",
+    description: "Endurance is a tool for generating conversation, code, images, videos and music using GPT-3.5 and replicate.ai.",
 };
 
 export default async function RootLayout({children}: { children: React.ReactNode }) {
